0cfe986268d4017bd36f1a7c8733d39db4ea1495
tinanano19 <lauratina98@gmail.com>
2022-09-01 12:26:27 -0500

    Completed

--- pokedex-advanced/src/NotFound/NotFound.jsx
+++ pokedex-advanced/src/NotFound/NotFound.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import {motion} from 'framer-motion'
-import img from "./pikasad.png"
-import './NotFound.css'
+import React from "react";
+import { motion } from "framer-motion";
+import img from "./pikasad.png";
+import "./NotFound.css";
 
-const NotFound = ({pokemon}) => {
+const NotFound = ({ pokemon }) => {
   return (
-    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} className="o-not">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      className="o-not"
+    >
+      <div className="col o-cont-img">
         <img src={img} alt="" />
-        <h2>Sorry we couldn't find "{pokemon}".</h2>
+      </div>
+      <h2>Sorry we couldn't find "{pokemon}".</h2>
     </motion.div>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
